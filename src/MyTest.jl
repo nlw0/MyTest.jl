f61eceea1eaf047f1b96a781df1fe89f94981fd9
Nicolau Leal Werneck <nwerneck@gmail.com>
2020-09-13 11:46:09 +0200

    removing seemingly unnecessary distortion transformation

--- src/MyTest.jl
+++ src/MyTest.jl
@@ -6,26 +6,11 @@ using CoordinateTransformations
 using LinearAlgebra: norm
 
 
-# struct HarrisDistortion{V} <: Transformation
-#     κ::V
-# end
-
-# function (H::HarrisDistortion{V})(x) where {V}
-#     χ = norm(x)
-#     g = max(0, 1 - 2 * H.κ * χ^2)^-(1//2)
-#     g * x
-# end
-
-# function Base.inv(H::HarrisDistortion)
-#     HarrisDistortion(-H.κ)
-# end
-
 function gendata()
     psim = randn(6)
     M = reshape(psim[1:4], 2,2)
     v = psim[4:5]
-    # Dist = HarrisDistortion(randn()*0.2)
-    T = AffineMap(M, v) #∘ Dist
+    T = AffineMap(M, v)
 
     Npt = 111
     xx = rand(2,Npt)
@@ -34,7 +19,7 @@ function gendata()
     xx, yy, psim
 end
 
-# result = optimize!(LeastSquaresProblem(x = p0, f! = residue!, output_length = prod(size(yy)), autodiff=:forward), LevenbergMarquardt())
+result = optimize!(LeastSquaresProblem(x = p0, f! = residue!, output_length = prod(size(yy)), autodiff=:forward), LevenbergMarquardt())
 
 function dofit(xx,yy)
     function residue!(out, p)
@@ -43,8 +28,7 @@ function dofit(xx,yy)
         OO = reshape(out, 2, :)
         M = reshape(p[1:4], 2,2)
         v = p[5:6]
-        # Dist = HarrisDistortion(p[7])
-        T = AffineMap(M,v) #∘ Dist
+        T = AffineMap(M,v)
         OO .= yy - mapslices(T, xx, dims=1)
         nothing
     end
